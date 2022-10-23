e2d09d3d9f9cdeaf64f70ff4d74500aee7bb54ae
Vladyslav <115419328+Sheparb@users.noreply.github.com>
2022-10-23 23:31:40 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public class Human
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int age { get; set; }
+            public string Surname { get; set; }
+        }
+        {
+        }
+    }
+}
